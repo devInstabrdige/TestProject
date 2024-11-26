dc10439c6ad251df9cff4d3b82e3b05b5379df54
devInstabrdige <dev.dhar@instabridge.com>
2024-11-26 16:25:10 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android 1",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
