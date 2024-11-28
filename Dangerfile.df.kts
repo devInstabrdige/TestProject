0cc9a9fedee2714932f13c3ee5f2ee76f6c9c2c8
devInstabrdige <dev.dhar@instabridge.com>
2024-11-28 15:53:49 +0100

    Update Dangerfile.df.kts

--- Dangerfile.df.kts
+++ Dangerfile.df.kts
@@ -7,5 +7,12 @@ danger(args) {
         if (!isTrivial) {
             warn("Please ensure this PR has been reviewed.")
         }
+
+        val jacocoReportPath = "app/build/reports/jacoco/jacocoTestReport.xml" // Adjust the path as necessary
+
+        Jacoco.report(jacocoReportPath) {
+            // Optional: Configure thresholds for coverage reporting
+            minCoveragePercentage = 80.0 // Set the minimum acceptable coverage percentage
+        }
     }
 }
